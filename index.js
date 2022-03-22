c11cbf245a0a002a711ac4a622078da3419da8ca
William Schultz <will627@comcast.net>
2022-03-22 13:29:00 -0400

    save note

---
+++ index.js
@@ -0,0 +1,3 @@
+// TODO: Add section links.
+// See https://frasertweedale.github.io/blog-fp/posts/2020-12-10-hakyll-section-links.html.
+let elems = document.querySelectorAll("h1, h2, h3, h4, h5, h6")
